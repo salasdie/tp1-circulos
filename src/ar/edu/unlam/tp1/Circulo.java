d2b7fa310afabc918bdcf760e366a76d9112d00e
salasdie <diegonsalas@gmail.com>
2016-04-19 17:08:35 -0300

    Tp-1 Modificado

--- src/ar/edu/unlam/tp1/Circulo.java
+++ src/ar/edu/unlam/tp1/Circulo.java
@@ -3,26 +3,31 @@ package ar.edu.unlam.tp1;
 public class Circulo{
 	private Double radio;
 		
+	public Circulo(Double radio) {
+		
+		this.radio= radio;
+	}
+
 	public Double getRadio(){
+		
 		return radio;
 	}
 	
 	public void setRadio(Double radio){
+		
 		this.radio= radio;
 	}
 	
 	public Double calcularPerimetro(){
 		
-        Double perimetro=0.00;
-		
-		perimetro = 2*Math.PI*getRadio();
+		Double perimetro = 2*Math.PI*this.radio;
 		
 		return perimetro;	
-}
+	}
 	public Double calcularArea(){
 		
-		Double area=0.00;
-		area=Math.PI*(getRadio()*getRadio());
+		Double area=Math.PI*this.radio*this.radio;
+		
 		return area;
-}
+	}
 }
--- test/ar/edu/unlam/tp1/CirculoTest.java
+++ test/ar/edu/unlam/tp1/CirculoTest.java
@@ -5,85 +5,82 @@ import org.junit.Test;
 
 public class CirculoTest {
 
-	
-	
-	@Test
-	public void crearUnCirculoConRadio2() {
-		Circulo circuloRadio2 = new Circulo();
-		Double radio = new Double(2.0);
-		circuloRadio2.setRadio(radio);
-		assertEquals(12.56, circuloRadio2.calcularPerimetro(), 0.01);
-	}
-	
-	@Test
-	public void crearUnCirculoConRadio3Punto7() {
-		Circulo circuloRadio3punto7 = new Circulo();
-		Double radio = new Double(3.7);
-		circuloRadio3punto7.setRadio(radio);
-		assertEquals(23.24, circuloRadio3punto7.calcularPerimetro(), 0.01);
-	}
-	
-	@Test
-	public void crearUnCirculoConRadio5() {
-		Circulo circuloRadio5 = new Circulo();
-		Double radio = new Double(5.0);
-		circuloRadio5.setRadio(radio);
-		assertEquals(31.41, circuloRadio5.calcularPerimetro(), 0.01);
-	}
-	
-	@Test
-	public void crearUnCirculoConRadio10Punto9() {
-		Circulo circuloRadio10Punto9 = new Circulo();
-		Double radio = new Double(10.9);
-		circuloRadio10Punto9.setRadio(radio);
-		assertEquals(68.48, circuloRadio10Punto9.calcularPerimetro(), 0.01);
-	}
-	
-   @Test
-	public void queElAreaDeUnCirculoConRadio9Punto8Es615Punto73() {
-		Circulo areaDeUnCirculoRadio9Punto8Es615Punto73 = new Circulo();
-		Double radio = new Double(9.8);
-		areaDeUnCirculoRadio9Punto8Es615Punto73.setRadio(radio);
-		assertNotEquals(615.73, areaDeUnCirculoRadio9Punto8Es615Punto73.calcularArea(), 0.01);
-	}
-   
-   @Test
-    public void queElAreaDeUnCirculoConRadio9Punto8Es301Punto71(){
-	   Circulo areaDeUnCirculoRadio9Punto8Es301Punto71 = new Circulo();
-	   Double radio = new Double(9.8);
-	   areaDeUnCirculoRadio9Punto8Es301Punto71.setRadio(radio);
-	   assertEquals(301.71, areaDeUnCirculoRadio9Punto8Es301Punto71.calcularArea(), 0.01);
-   }
-	
-	@Test
-	public void crearUnCirculoConRadio16Punto6Es865Punto67() {
-		Circulo circuloRadio16Punto6 = new Circulo();
-		Double radio = new Double(16.6);
-		circuloRadio16Punto6.setRadio(radio);
-		assertNotEquals(865.67, circuloRadio16Punto6.calcularPerimetro(), 0.01);
-	}
-	
-	@Test
-	 public void queElAreaDeUnCirculoConRadio16Punto6Es865Punto69(){
-		Circulo areaDeUnCirculoRadio16Punto6 = new Circulo();
-		Double radio = new Double(16.6);
-		areaDeUnCirculoRadio16Punto6.setRadio(radio);
-		assertEquals(865.69, areaDeUnCirculoRadio16Punto6.calcularArea(), 0.01);
-	}
-	
-	@Test
-	public void queElTipDeAyudaDeVerde(){ // :)
+		@Test
+		public void crearUnCirculoConRadio2() {
+			Circulo micirculo = new Circulo(2.0);
+			Double esperado = 2.0;
+			Double actual = micirculo.getRadio();
+			assertEquals(esperado.doubleValue(), actual.doubleValue(), 0.01);
+			}
 		
-		Double esperado = 1.10;
-		Double actual = 1.10;
+		@Test
+		public void crearUnCirculoConRadio3Punto7() {
+			Circulo micirculo = new Circulo(3.7);
+			Double esperado = 3.7;
+			Double actual = micirculo.getRadio();
+			assertEquals(esperado.doubleValue(), actual.doubleValue(), 0.01);
+			}
 		
-		/**
-		 *  Recordar que el siguiente método se puede usar por que al inicio del archivo
-		 *  se importaron todos los assert con la siguiente linea 
-		 *  import static org.junit.Assert.*; 
-		 */
-		 
-		assertEquals(esperado.doubleValue(), actual.doubleValue(),0.01);
-	}
+		@Test
+		public void crearUnCirculoConRadio5() {
+			Circulo micirculo = new Circulo(5.0);
+			Double esperado = 5.0;
+			Double actual = micirculo.getRadio();
+			assertEquals(esperado.doubleValue(), actual.doubleValue(), 0.01);
+			}
+		
+		@Test
+		public void crearUnCirculoConRadio10Punto9() {
+			Circulo micirculo = new Circulo(10.9);
+			Double esperado = 10.9;
+			Double actual = micirculo.getRadio();
+			assertEquals(esperado.doubleValue(), actual.doubleValue(), 0.01);
+			}
+		
+		@Test
+		public void queElAreaDeUnCirculoConRadio9Punto8Es301Punto71() {
+			Circulo micirculo = new Circulo(9.8);
+			Double esperado = 301.71;
+			Double actual = micirculo.calcularArea();
+			assertEquals(esperado.doubleValue(), actual.doubleValue(), 0.01);
+			}
+		
+		@Test
+		public void queElPerimetroDeUnCirculoConRadio9Punto8Es615Punto73() {
+			Circulo micirculo = new Circulo(9.8);
+			Double esperado = 61.57; 
+			Double actual = micirculo.calcularPerimetro();	
+			assertEquals(esperado.doubleValue(), actual.doubleValue(), 0.01);
+			}
+		
+		@Test
+		public void queElAreaDeUnCirculoConRadio16Punto6Es865Punto69() {
+			Circulo micirculo = new Circulo(16.6);
+			Double esperado = 865.69; 
+			Double actual = micirculo.calcularArea();
+			assertEquals(esperado.doubleValue(), actual.doubleValue(), 0.01);
+			}
+		
+		@Test
+		public void queElPerimetroDeUnCirculoConRadio16Punto6Es865Punto67() {
+			Circulo micirculo = new Circulo(16.6);
+			Double esperado = 104.30;
+			Double actual = micirculo.calcularPerimetro();
+			assertEquals(esperado.doubleValue(), actual.doubleValue(), 0.01);
+			}
 	
-}
+		@Test
+		public void queElTipDeAyudaDeVerde(){ // :)
+			Double esperado = 1.10;
+			Double actual = 1.10;
+			assertEquals(esperado.doubleValue(), actual.doubleValue(),0.01);
+			}
+	
+	}
+
+/**
+ *  Recordar que el siguiente método se puede usar por que al inicio del archivo
+ *  se importaron todos los assert con la siguiente linea 
+ *  import static org.junit.Assert.*; 
+ */
+ 
